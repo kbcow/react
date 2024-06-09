9f673ad5c124c7d42a3a92ea7ecdf44e3cbc46ec
AD1065 <ad1065@AD1065ui-MacBookPro.local>
2024-06-09 18:26:37 +0900

    렌더링된useRef로체크

--- src/App.js
+++ src/App.js
@@ -1,33 +1,25 @@
 import "./App.css";
-import { useState, useRef } from "react";
+import { useState, useRef, useEffect } from "react";
 
 function App() {
-  const [renderer, setrenderer] = useState(0);
-  const countRef = useRef(0);
-  // 렌더링이 되면 countVar는 다시 0 으로 바뀌기 때문에
-  // ref는 숫자가 올라가지만 var는 다시 0으로 초기화됨
-  let countVar = 0;
+  const [count, setCount] = useState(1);
+  const useRecord = useRef(0);
+  useEffect(() => {
+    useRecord.current = useRecord.current + 1;
+    console.log("렌더링 수 : ", useRecord.current);
+  });
 
-  const increasRef = () => {
-    countRef.current = countRef.current + 1;
-    console.log("ref : ", countRef.current);
-  };
-
-  const increasVar = () => {
-    countVar = countVar + 1;
-    console.log("var : ", countVar);
-  };
-  const doRender = () => {
-    setrenderer(renderer + 1);
-  };
-
+  console.log(useRecord);
   return (
     <div>
-      <p>Ref : {countRef.current} </p>
-      <p>Ref : {countVar} </p>
-      <button onClick={doRender}>렌더! </button>
-      <button onClick={increasRef}>Ref 올려 </button>
-      <button onClick={increasVar}>변수 올려 </button>
+      <p>Count : {count}</p>
+      <button
+        onClick={() => {
+          setCount(count + 1);
+        }}
+      >
+        count올려
+      </button>
     </div>
   );
 }
