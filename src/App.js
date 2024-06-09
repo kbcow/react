a7c2f5be4c072a88d1b31d7304c43310d24de200
AD1065 <ad1065@AD1065ui-MacBookPro.local>
2024-06-09 17:46:39 +0900

    useEffect

--- src/App.js
+++ src/App.js
@@ -1,32 +1,14 @@
 import "./App.css";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import Timer from "./components/Timer";
 
-const heavyWrok = () => {
-  console.log("엄청 무거운 작업");
-  return ["홍길동", "김병우"];
-};
 function App() {
-  const [names, setNames] = useState(() => {
-    return heavyWrok();
-  });
-  const [input, setInput] = useState("");
-
-  const handleInputChange = (e) => {
-    setInput(e.target.value);
-  };
-  const pushOnButton = () => {
-    setNames((i) => {
-      return [input, ...i];
-    });
-  };
+  const [showTimer, setShowTimer] = useState(false);
 
   return (
     <div>
-      <input type="text" value={input} onChange={handleInputChange} />
-      <button onClick={pushOnButton}> Upload</button>
-      {names.map((name, idx) => {
-        return <p key={idx}>{name}</p>;
-      })}
+      {showTimer && <Timer />}
+      <button onClick={() => setShowTimer(!showTimer)}>Toggle Timer</button>
     </div>
   );
 }
