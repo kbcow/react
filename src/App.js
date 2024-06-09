3eda83209ab7ff777313c75dc4fa8879709f895c
AD1065 <ad1065@AD1065ui-MacBookPro.local>
2024-06-09 18:18:59 +0900

    useRef와 변수의 차이

--- src/App.js
+++ src/App.js
@@ -1,8 +1,35 @@
 import "./App.css";
-import { useState } from "react";
+import { useState, useRef } from "react";
 
 function App() {
-  return <div></div>;
+  const [renderer, setrenderer] = useState(0);
+  const countRef = useRef(0);
+  // 렌더링이 되면 countVar는 다시 0 으로 바뀌기 때문에
+  // ref는 숫자가 올라가지만 var는 다시 0으로 초기화됨
+  let countVar = 0;
+
+  const increasRef = () => {
+    countRef.current = countRef.current + 1;
+    console.log("ref : ", countRef.current);
+  };
+
+  const increasVar = () => {
+    countVar = countVar + 1;
+    console.log("var : ", countVar);
+  };
+  const doRender = () => {
+    setrenderer(renderer + 1);
+  };
+
+  return (
+    <div>
+      <p>Ref : {countRef.current} </p>
+      <p>Ref : {countVar} </p>
+      <button onClick={doRender}>렌더! </button>
+      <button onClick={increasRef}>Ref 올려 </button>
+      <button onClick={increasVar}>변수 올려 </button>
+    </div>
+  );
 }
 
 export default App;
